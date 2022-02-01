1b0cbb45945f838bd7f1945a2d53680a0e34089e
DenisAkinshin <akinshin2022@list.ru>
2022-02-01 14:57:38 +0300

    HotFixe

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,3 @@
-// Likes
-
-let likes = document.querySelectorAll('.element__like');
-
-for (let index = 0; index<likes.length; index++){
-    let like = likes[index];
-    like.addEventListener("click", () => 
-        like.classList.toggle('active')
-    );
-}
-
-
-// Popup open and close
-
-let popupOpen = document.querySelector('.profile__edit-button'),
-    popupClose = document.querySelector('.popup__close'),
-    popup = document.querySelector('.popup');
-
-popupOpen.addEventListener('click', ()=>{
-    popup.classList.add('popup_opened');
-})
-
-popupClose.addEventListener('click', ()=>{
-    popup.classList.remove('popup_opened');
-})
-
-
 // Form
 
 let formElement = document.querySelector('.form'),
@@ -35,9 +8,32 @@ let formElement = document.querySelector('.form'),
 
 function formSubmitHandler (evt) { 
     evt.preventDefault();
-    popup.classList.remove('popup_opened');
+    closePopup();
     valueInfo.textContent = nameInput.value;
     valueBusiness.textContent = jobInput.value;
 };
 
 formElement.addEventListener('submit', formSubmitHandler);
+
+
+// Popup open and close
+
+let popupOpen = document.querySelector('.profile__edit-button'),
+    popupClose = document.querySelector('.popup__close'),
+    popup = document.querySelector('.popup');
+
+function openPopup(){
+    popup.classList.add('popup_opened');
+    nameInput.value = valueInfo.innerText;
+    jobInput.value = valueBusiness.innerText;
+}    
+
+function closePopup(){
+    popup.classList.remove('popup_opened');
+} 
+
+popupOpen.addEventListener('click', openPopup);
+
+popupClose.addEventListener('click',closePopup);
+
+
