b0114f167c2b554930c632ede9e25e4c6819c585
DenisAkinshin <akinshin2022@list.ru>
2022-02-25 22:03:12 +0300

    final

--- scripts/index.js
+++ scripts/index.js
@@ -9,19 +9,19 @@ function closePopup() {
             popupElement.classList.remove('popup_opened');
         }
     })
-    document.removeEventListener('keydown',closeEscHandle.bind);
-    document.removeEventListener('mousedown', closePopupMousedown);
+    document.removeEventListener('keydown',handleEscKey);
+    document.removeEventListener('mousedown', handleOverlayClick);
 }
 
 
-function closeEscHandle(evt) {
+function handleEscKey(evt) {
     console.log(evt);
     if (evt.key === 'Escape') {
         closePopup();
     }
 }
 
-function closePopupMousedown(evt) {
+function handleOverlayClick(evt) {
     if (evt.target.classList.contains('popup_opened')) {
         closePopup();
     }
@@ -48,19 +48,19 @@ popupOpenProfile.addEventListener('click', openProfilePopup);
 
 function openPopup(element) {
     element.classList.add('popup_opened');
-    document.addEventListener('keydown',closeEscHandle);
-    document.addEventListener('mousedown', closePopupMousedown);
+    document.addEventListener('keydown',handleEscKey);
+    document.addEventListener('mousedown', handleOverlayClick);
 }
 
 
-const formProfileElement = document.querySelector('#description-form'),
-    nameInput = document.querySelector('#input-name'),
-    jobInput = document.querySelector('#input-business'),
-    nameInfo = document.querySelector('.profile__title'),
-    jobInfo = document.querySelector('.profile__description'),
-    formInputsProfile = [nameInput,jobInput],
+const formProfileElement = document.querySelector('#description-form');
+const nameInput = document.querySelector('#input-name');
+const jobInput = document.querySelector('#input-business');
+const nameInfo = document.querySelector('.profile__title');
+const jobInfo = document.querySelector('.profile__description');
+const formInputsProfile = [nameInput,jobInput];
 
-function submitFormProfileHandler (evt) { 
+function handleProfileFormSubmit (evt) { 
     console.log(formInputsProfile);  
     evt.preventDefault();
     nameInfo.textContent = nameInput.value;
@@ -69,19 +69,19 @@ function submitFormProfileHandler (evt) {
     closePopup();
 };
 
-formProfileElement.addEventListener('submit',submitFormProfileHandler);
+formProfileElement.addEventListener('submit',handleProfileFormSubmit);
 
 
 //Form cards
 
-const formCardsElement = document.querySelector('#cards-form'),
-    popupCards = document.querySelector('#cards'),
-    inputCard = document.querySelector('#input-card'),
-    inputImage = document.querySelector('#input-image'),
-    popupOpenCards = document.querySelector('.profile__add-button'),
-    elements = document.querySelector('.elements');
+const formCardsElement = document.querySelector('#cards-form');
+const popupCards = document.querySelector('#cards');
+const inputCard = document.querySelector('#input-card');
+const inputImage = document.querySelector('#input-image');
+const popupOpenCards = document.querySelector('.profile__add-button');
+const elements = document.querySelector('.elements');
 
-function submitFormCardsHandler (evt) {
+function handleCardsFormSubmit (evt) {
     evt.preventDefault();
 
     const item = {};
@@ -97,7 +97,7 @@ function submitFormCardsHandler (evt) {
 
 
 
-formCardsElement.addEventListener('submit',submitFormCardsHandler);
+formCardsElement.addEventListener('submit',handleCardsFormSubmit);
 
 popupOpenCards.addEventListener('click',function() {
     openPopup(popupCards);
@@ -111,7 +111,7 @@ const imageTitle = document.querySelector('.image__title');
 const imagePopup = document.querySelector('#image');
 
 
-function openImageHandle (item) {
+function openImagePopup (item) {
     imageBody.src = item.target.src;
     imageBody.alt = item.target.alt;
     imageTitle.textContent = item.target.alt;
@@ -177,7 +177,7 @@ function createCard (item) {
     });
 
     cardDeleteButton.addEventListener('click',deleteCard);
-    cardImage.addEventListener('click',openImageHandle);
+    cardImage.addEventListener('click',openImagePopup);
 
     return cardElement;
 }
