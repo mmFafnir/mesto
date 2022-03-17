c1802c8046f5dabade6be777ca8f9749d38cbc12
ivan1981sololov <94713393+ivan1981sololov@users.noreply.github.com>
2022-03-17 13:40:57 +0400

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -8,12 +8,14 @@ const popupList = Array.from(document.querySelectorAll('.popup'));
 
 
 
-function closePopup() {
-    popupList.forEach((popupElement) => {
-        if (popupElement.classList.contains('popup_opened')) {
-            popupElement.classList.remove('popup_opened');
-        }
-    })
+function closePopup(popup) {
+    console.log(popup)
+    popup.classList.remove('popup_opened')
+    // popupList.forEach((popupElement) => {
+    //     if (popupElement.classList.contains('popup_opened')) {
+    //         popupElement.classList.remove('popup_opened');
+    //     }
+    // })
     document.removeEventListener('keydown',handleEscKey);
     document.removeEventListener('mousedown', handleOverlayClick);
 }
@@ -21,13 +23,14 @@ function closePopup() {
 
 function handleEscKey(evt) {
     if (evt.key === 'Escape') {
-        closePopup();
+        const activePopup = document.querySelector('.popup_opened')
+        closePopup(activePopup);
     }
 }
 
 function handleOverlayClick(evt) {
     if (evt.target.classList.contains('popup_opened')) {
-        closePopup();
+        closePopup(evt.target.closest('.popup_opened'));
     }
 }
 
@@ -69,8 +72,9 @@ function handleProfileFormSubmit (evt) {
     evt.preventDefault();
     nameInfo.textContent = nameInput.value;
     jobInfo.textContent = jobInput.value;
-       
-    closePopup();
+
+    const activePopup = evt.target.closest('.popup_opened') 
+    closePopup(activePopup);
 };
 
 formProfileElement.addEventListener('submit',handleProfileFormSubmit);
@@ -97,7 +101,9 @@ function handleCardsFormSubmit (evt) {
 
     inputCard.value = '';
     inputImage.value = '';
-    closePopup();
+
+    const activePopup = document.querySelector('.popup_opened')
+    closePopup(activePopup);
 };
 
 
@@ -171,7 +177,8 @@ downloadCards();
 const closePopupButtonList = Array.from(document.querySelectorAll('.popup__close'));
 
 closePopupButtonList.forEach((buttonElement) => {
-    buttonElement.addEventListener('click',closePopup)
+    const activePopup = buttonElement.closest('.popup')
+    buttonElement.addEventListener('click',() =>  closePopup(activePopup))
 })
 
 
