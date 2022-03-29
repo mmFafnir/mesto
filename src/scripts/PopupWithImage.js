63b0f67f45ff68f675a6bfcb1dde921fc5c4bc07
ivan1981sololov <94713393+ivan1981sololov@users.noreply.github.com>
2022-03-29 20:02:45 +0400

    Update PopupWithImage.js

--- src/scripts/PopupWithImage.js
+++ src/scripts/PopupWithImage.js
@@ -4,25 +4,17 @@ import Popup from './Popup.js'
 export default class PopupWithImage extends Popup {
     
     constructor(selector){
-        // this.popup = document.querySelector(selector);
-        // this._handleEscClose = this._handleEscClose.bind(this)
         super(selector);
+
         this.image = this.popup.querySelector('.image__body');
         this.title = this.popup.querySelector('.image__title');
     }
 
     open(item) {
-    // super()        
-    
         this.image.src = item.src;
         this.image.alt = item.alt;
         this.title.textContent = item.alt;
 
         super.open( );
-        // console.log(super())
-        // this.popup.classList.add('popup_opened');
-
-        // document.addEventListener('keydown', this._handleEscClose)
-        // this._handleEscClose();
-    }   
+    }
 }
