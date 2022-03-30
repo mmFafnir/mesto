cfea2fbc6ec416e2968ba51e4cff4bfcef6bb20d
ivan1981sololov <94713393+ivan1981sololov@users.noreply.github.com>
2022-03-31 02:01:34 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 
-import './pages/index.css'; 
+import './pages/index.css';
 
 import Section from './scripts/Section.js'
 import Card from './scripts/Card.js'
@@ -23,17 +23,17 @@ const handleCardClick = (item) => {
   popupImages.open(item);
 }
 
-const createCard = (item) => {  
+const createCard = (item) => {
   const card = new Card(item, '#element-template', handleCardClick);
   return card.createCard()
 }
 
-  
+
 let cardsSection; //Создаю переменную cardsSection, для того чтобы получить к ней доступ во всем документе.
 
 function downloadCards () {
 
-        let templateCatds = []  
+        let templateCatds = []
 
         initialCards.forEach((item) => {
           templateCatds.push(createCard(item));
@@ -45,25 +45,22 @@ function downloadCards () {
                 container.prepend(item);
             }
         }, '.elements')
-        
-        cardsSection.renderAll();  
+
+        cardsSection.renderAll();
 }
 
 const popupImages = new PopupWithImage('#image');
-popupImages.setEventListeners();  
+popupImages.setEventListeners();
 const popupUser = new PopupWithForm('#description', () => {
-    const values = popupUser.getInputValues() 
+    const values = popupUser.getInputValues()
     user.setUserInfo(values.Title, values.Business)
 });
 popupUser.setEventListeners();
 
 const popupAddCard = new PopupWithForm('#cards', function(){
-  
+
   const item = this.getInputValues()
-  console.log(item)
-  const card = new Card(item, '#element-template', handleCardClick)
-  
-  cardsSection.addItem(card.createCard())
+  cardsSection.addItem(createCard(item))
 })
 popupAddCard.setEventListeners();
 downloadCards();
@@ -90,14 +87,14 @@ const jobInput = document.querySelector('#input-business');
 btnPopupUser.addEventListener('click', () => {
     popupUser.open();
 
-    const userInf = user.getUserInfo() 
+    const userInf = user.getUserInfo()
     nameInput.value = userInf.name.trim();
     jobInput.value = userInf.about.trim();
 })
 
 btnPopupCard.addEventListener('click', () => {
   popupAddCard.open();
-  
+
 })
 
 
