43d6c2b830b9a602838715a2e2a394bc4f7d2a33
ivan1981sololov <94713393+ivan1981sololov@users.noreply.github.com>
2022-04-14 12:37:33 +0400

    Update PopupNotify.js

--- src/components/PopupNotify.js
+++ src/components/PopupNotify.js
@@ -5,7 +5,7 @@ export default class PopupNotify extends Popup {
     constructor(select, callback) {
         super(select);
         this.callback = callback
-        this.btnSubmit = this.btnSubmit = this.popup.querySelector('.form__submit');
+        this.btnSubmit = this.popup.querySelector('.form__submit');
     }
 
     open(id){
@@ -16,14 +16,9 @@ export default class PopupNotify extends Popup {
 
     setEventListeners() {
         super.setEventListeners();
-        this.btnSubmit.addEventListener('click', async (evt) => {
+            this.btnSubmit.addEventListener('click', (evt) => {
             evt.preventDefault()
-            await this.callback();
-            await document.getElementById(this.id).remove()
-            await this.close();
-
+            this.callback();
         })
     }
-
-
 }
