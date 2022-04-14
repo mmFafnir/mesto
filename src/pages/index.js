ad3ee9b941b4295e43e99c18691f2377a7aec187
ivan1981sololov <94713393+ivan1981sololov@users.noreply.github.com>
2022-04-14 12:39:22 +0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,45 +13,32 @@
 
     import UserInfo from '../components/UserInfo.js';
 
-    import { initialCards, selectorsForm } from '../utils/constants'
+    import { selectorsForm } from '../utils/constants';
 
     const user = new UserInfo({
-
       name: '.profile__title',
       about: '.profile__description',
       avatar: '.profile__avatar'
     })
 
-    const cardsApi = new Api({
-        baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-39/cards',
+    const api = new Api({
+        baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-39/',
         headers: {
             authorization: '446e7ea3-0df9-437f-a6c2-d22adf8a9199',
             'Content-Type': 'application/json'
         }
     })
 
-    const userApi = new Api({
-        baseUrl: 'https://nomoreparties.co/v1/cohort-39/users/me',
-        headers: {
-            authorization: '446e7ea3-0df9-437f-a6c2-d22adf8a9199',
-            'Content-Type': 'application/json'
-        }
-    })
-    async function fetchUser  () {
-        const userObj = await userApi.getInitial();
-        const userData = await userObj.data
-        if(userObj.status){
-            user.id = await userData._id;
-            await user.setUserInfo(
-                userData.name,
-                userData.about,
-
-            )
-            await user.setUserAvatar(userData.avatar)
-
-        }
+     function initialUser  (userData) {
+        user.setUserInfo(
+            userData.name,
+            userData.about,
+            userData.avatar,
+            userData._id,
+        )
+        user.setUserAvatar(userData.avatar)
     }
-    fetchUser()
+
 
 
 
@@ -60,84 +47,105 @@
     }
 
     const createCard = (item) => {
-      const card = new Card(item, '#element-template', handleCardClick, popupCardDelete, user, cardsApi);
+      const card = new Card(item, '#element-template', handleCardClick, popupCardDelete, user, api);
       return card.createCard()
     }
 
 
     let cardsSection; //Создаю переменную cardsSection, для того чтобы получить к ней доступ во всем документе.
-
-    //console.log(cardsApi.getInitialCards)
-
-    async function downloadCards () {
-
-            let templateCatds = [];
-            const cards = await cardsApi.getInitial();
-            const cardsData = await cards.data
-
-            if(cards.status){
-                await cardsData.forEach((item) => {
-                  templateCatds.push(createCard(item));
-                })
-            }
-
-            cardsSection = await new Section({
+    function initialCards (cards) {
+            const templateCatds = [];
+            cards.forEach((item) => {
+              templateCatds.push(createCard(item));
+            })
+            cardsSection = new Section({
                 items: templateCatds,
                 renderer: (item, container) => {
                     container.prepend(item);
                 }
             }, '.elements')
-
-            await cardsSection.renderAll();
+            cardsSection.renderAll();
     }
+
+
+    Promise.all([api.getUserData(), api.getInitialCards()])
+        .then(([userData, cards]) => {
+            initialUser(userData);
+            initialCards(cards)
+        })
+
+
 
     const popupImages = new PopupWithImage('#image');
     popupImages.setEventListeners();
-    const popupUser = new PopupWithForm('#description', async function () {
-        const values = await popupUser.getInputValues();
-        const userPatch = await userApi.PATCH({
+
+    const popupUser = new PopupWithForm('#description', function () {
+        const values = popupUser.getInputValues();
+         api.updateUserInfo({
            name: values.Title,
            about: values.Business
-        });
-        if(userPatch.status){
-            await user.setUserInfo(values.Title, values.Business);
-            return true
-        }
+        })
+        .then((userData) => {
+            user.setUserInfo(userData.name, userData.about, userData.avatar, userData._id);
+            popupUser.close();
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+        .finally(() => {
+            popupUser.btnSubmit.disabled = false;
+            popupUser.btnSubmit.textContent = 'Сохранить'
+        })
 
     });
     popupUser.setEventListeners();
 
-    const popupAvatar = new PopupWithForm('#popup-avatar', async function(){
-        const value = await popupAvatar.getInputValues();
-        const avatarPatch = await userApi.PATCH({
+    const popupAvatar = new PopupWithForm('#popup-avatar',function(){
+        const value = popupAvatar.getInputValues();
+        api.updateAvatar({
             avatar: value.link
-        }, 'avatar');
-        if(avatarPatch.status) {
-            await user.setUserAvatar(value.link);
-            return true
-        }
+        }).then((userData) => {
+            user.setUserAvatar(userData.avatar);
+            popupAvatar.close();
+        })
+        .catch((err) => {
+            console.log(err)
+            return false
+        })
+        .finally(() => {
+            popupAvatar.btnSubmit.disabled = false;
+            popupAvatar.btnSubmit.textContent = 'Сохранить'
+        })
     });
     popupAvatar.setEventListeners()
 
     const popupCardDelete = new PopupNotify('#delete-notify', function() {
-        cardsApi.DELETE(popupCardDelete.id)
+        api.deleteCard(popupCardDelete.id)
+        .then(() => {
+            document.getElementById(popupCardDelete.id).remove()
+            popupCardDelete.close();
+        })
+        .catch((e) => console.log(e))
     });
     popupCardDelete.setEventListeners();
 
 
-    const popupAddCard = new PopupWithForm('#cards', async function (){
-      const {name, link} = await popupAddCard.getInputValues();
-      const item = await cardsApi.POST({
-        name, link
-      });
-      if(item.status){
-        await cardsSection.addItem(createCard(item.data));
-        return true
-      }
-      return false
+    const popupAddCard = new PopupWithForm('#cards', function (){
+      const {name, link} = popupAddCard.getInputValues();
+      api.addCard({name, link})
+      .then((item) => {
+        cardsSection.addItem(createCard(item));
+        popupAddCard.close();
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        popupAddCard.btnSubmit.disabled = false;
+        popupAddCard.btnSubmit.textContent = 'Сохранить';
+      })
     })
     popupAddCard.setEventListeners();
-    downloadCards();
 
 
     const btnPopupUser = document.querySelector('.profile__edit-button');
@@ -165,10 +173,11 @@
 
 
 
-    const form1 = new FormValidator(selectorsForm, '.form-one');
-    form1.enableValidation()
-    const form2 = new FormValidator(selectorsForm, '.form-two');
-    form2.enableValidation()
-
+    const validatorProfile = new FormValidator(selectorsForm, '.form-one');
+    validatorProfile.enableValidation()
+    const validatorCards = new FormValidator(selectorsForm, '.form-two');
+    validatorCards.enableValidation()
+    const validatorAvatar = new FormValidator(selectorsForm, '#popup-avatar');
+    validatorAvatar.enableValidation()
 
 
